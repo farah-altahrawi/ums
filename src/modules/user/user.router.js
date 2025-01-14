d3eda3fd3966dccf5d7379f9ad23d8e06d642ff0
farah-altahrawi <faltahrawy@gmai.com>
2025-01-14 15:08:32 +0300

    create login end point

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 
 
 import UserModel from '../../../DB/model/user.model.js';
@@ -18,5 +20,25 @@ router.post('/',async(req,res)=>{
 
     return res.status(201).json({message:"success"});
 });
+
+router.post('/login',async(req,res)=>{
+    const {email,password} = req.body;
+    const user = await UserModel.findOne({
+        where:{email:email}
+    });
+
+    if(user==null){
+        return res.status(404).json({message:"invalid email"});
+    }
+
+    const check = await bcrypt.compareSync(password,user.password);
+    if(check==false){
+        return res.status(400).json({message:"invalid password"});
+    }
+
+    const token = jwt.sign({id:user.id,name:user.userName}, 'farah');
+    return res.status(200).json({message:"success",token});
+})
+
 
 export default router; 
