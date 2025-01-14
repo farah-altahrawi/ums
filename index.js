f2b03cdc467faff9cf4207ef70888ed564af1afa
farah-altahrawi <faltahrawy@gmai.com>
2025-01-14 12:25:32 +0300

    create register end point

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { connectDB } from './DB/connection.js';
-import userRouter from './src/modules/user/user.js'
+import userRouter from './src/modules/user/user.router.js'
 const app = express();
 connectDB();
 
